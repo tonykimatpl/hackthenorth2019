a6e6d98ad9e30b80f6d8baa59f0ce7d278c000b5
Tony Kim <tonykimatpl@gmail.com>
2019-09-14 14:57:51 -0400

    debug and file system cleaning

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,8 +7,6 @@ import { withFirebase } from '../Firebase'
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
-import SignUpPage from '../SignUp';
-import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
