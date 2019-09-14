1f19f03e17658252bfda33711691096622eddc74
Tony Kim <tonykimatpl@gmail.com>
2019-09-14 14:01:03 -0400

    copyright

--- src/components/SignUpUI/index.js
+++ src/components/SignUpUI/index.js
@@ -31,7 +31,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Exam Goat
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
